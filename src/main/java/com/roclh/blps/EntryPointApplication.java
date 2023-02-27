b088c5e4e1d3293f70fe7ebfff3330918262f25e
Roclh <247666@itmo.ru>
2023-02-27 20:11:16 +0300

    Rename of main class

--- src/main/java/com/roclh/blps/EntryPointApplication.java
+++ src/main/java/com/roclh/blps/EntryPointApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlpsApplication {
+public class EntryPointApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlpsApplication.class, args);
+        SpringApplication.run(EntryPointApplication.class, args);
     }
 
 }
