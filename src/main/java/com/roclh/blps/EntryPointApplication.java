d07ca121125e2c93839a612388757e721fc38337
Roclh <247666@itmo.ru>
2023-02-28 23:30:58 +0300

    First view at controllers

--- src/main/java/com/roclh/blps/EntryPointApplication.java
+++ src/main/java/com/roclh/blps/EntryPointApplication.java
@@ -1,12 +1,17 @@
 package com.roclh.blps;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EntryPointApplication {
 
+    private static final Logger log = LogManager.getLogger(EntryPointApplication.class);
+
     public static void main(String[] args) {
+        log.info("Pussy tight pussy clean pussy fresh");
         SpringApplication.run(EntryPointApplication.class, args);
     }
 
--- src/main/java/com/roclh/blps/controllers/StudopediaController.java
+++ src/main/java/com/roclh/blps/controllers/StudopediaController.java
@@ -1,8 +1,46 @@
 package com.roclh.blps.controllers;
 
+import com.roclh.blps.entities.StudopediaArticle;
+import com.roclh.blps.service.StudopediaService;
+import com.roclh.blps.utils.HttpResponseErrorMessages;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
+
 
 @Controller
+@ApiResponses({
+        @ApiResponse(code = HttpServletResponse.SC_BAD_REQUEST, message = HttpResponseErrorMessages.BAD_REQUEST)
+})
 public class StudopediaController {
+
+    private static final Logger log = LoggerFactory.getLogger(StudopediaController.class);
+    private final StudopediaService service;
+
+    @Inject
+    public StudopediaController(StudopediaService service){
+        log.info("Initializing Studopedia Controller");
+        this.service = service;
+    }
+
+    @RequestMapping(path = "api/article/get-article", method = RequestMethod.POST)
+    @ResponseBody
+    @ApiOperation(value = "Get article by name")
+    public StudopediaArticle getArticle(@RequestBody @ApiParam("Article name") String articleName,
+                                        HttpServletResponse response){
+        log.info("Received request to get article with name {}", articleName);
+        return service.getArticle(articleName);
+    }
 
 }
